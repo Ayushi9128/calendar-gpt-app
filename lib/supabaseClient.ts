76452fd9bcbe31988f0a3d261f9592f74769a612
Ayushi1298 <ayushij2005eenu@gmail.com>
2025-06-24 21:07:28 +0530

    updated

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,8 +1,11 @@
 // lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+console.log('Supabase URL:', supabaseUrl); 
+console.log('Supabase Key:', supabaseKey);
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Supabase URL and Anon Key are required');
